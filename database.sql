e2fc44e3bcd304ea88825ea565e33668149540c6
BHD1673 <banhaiduong167@gmail.com>
2023-11-11 16:56:25 +0700

    Sửa lại database

--- database.sql
+++ database.sql
@@ -11,7 +11,7 @@ CREATE TABLE phong (
     so_phong VARCHAR(10) NOT NULL,
     loai_phong VARCHAR(50) NOT NULL,
     gia DECIMAL(10, 2) NOT NULL,
-    tinh_trang BOOLEAN DEFAULT false,
+    tinh_trang BOOLEAN DEFAULT true,
     ma_loai_phong INT,
     FOREIGN KEY (ma_loai_phong) REFERENCES loai_phong(ma_loai_phong)
 );
@@ -23,6 +23,28 @@ CREATE TABLE dich_vu_phong (
     gia DECIMAL(10, 2) NOT NULL
 );
 
+-- Bảng cho Khách Hàng
+CREATE TABLE khach_hang (
+    ma_khach_hang INT PRIMARY KEY AUTO_INCREMENT,
+    ten_khach_hang VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    so_dien_thoai VARCHAR(20) NOT NULL
+);
+
+-- Bảng cho Đặt Phòng
+CREATE TABLE dat_phong (
+    ma_dat_phong INT PRIMARY KEY AUTO_INCREMENT,
+    ma_phong INT,
+    ma_khach_hang INT,
+    ngay_checkin DATE NOT NULL,
+    ngay_checkout DATE NOT NULL,
+    so_nguoi_o INT NOT NULL,
+    hinh_anh TEXT, -- Dùng kiểu TEXT để lưu trữ đường dẫn đến hình ảnh
+    so_tien_dat_coc DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (ma_phong) REFERENCES phong(ma_phong),
+    FOREIGN KEY (ma_khach_hang) REFERENCES khach_hang(ma_khach_hang)
+);
+
 -- Bảng cho Hóa đơn
 CREATE TABLE hoa_don (
     ma_hoa_don INT PRIMARY KEY AUTO_INCREMENT,
@@ -32,7 +54,7 @@ CREATE TABLE hoa_don (
     FOREIGN KEY (ma_dat_phong) REFERENCES dat_phong(ma_dat_phong)
 );
 
--- Bảng nối cho Dịch vụ Phòng trong một đặt phòng
+-- Bảng nối cho Dịch vụ Phòng trong một Đặt Phòng
 CREATE TABLE dat_phong_dich_vu (
     ma_dat_phong INT,
     ma_dich_vu INT,
@@ -40,8 +62,3 @@ CREATE TABLE dat_phong_dich_vu (
     FOREIGN KEY (ma_dat_phong) REFERENCES dat_phong(ma_dat_phong),
     FOREIGN KEY (ma_dich_vu) REFERENCES dich_vu_phong(ma_dich_vu)
 );
-
-ALTER TABLE dat_phong
-ADD COLUMN ten_khach_hang VARCHAR(100),
-ADD COLUMN tien_dat_coc DECIMAL(10, 2),
-ADD COLUMN anh_khach_hang VARCHAR(255); -- Lưu đường dẫn đến ảnh khách hàng
