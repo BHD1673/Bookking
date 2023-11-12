f38cc4ba4852443425b93d53afab8027446c5e72
BHD1673 <banhaiduong167@gmail.com>
2023-11-12 17:19:13 +0700

    Vá lại file index khách hàng

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include "view/head.php";
+include "view/loader.php";
+include "view/header.php";
+include 'view/banner.php';
+include "view/review.php";
+include "view/room.php";
+include "view/gallery.php";
+include "view/footer.php";
+?>
