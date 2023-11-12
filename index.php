3304782962dd9d08a7a9ba8d3167ba6302bf39c2
DangThienDEV <thiendvph42781@fpt.edu.vn>
2023-11-12 17:02:42 +0700

    thien
    code comit

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-    include "index.test.php";
+include "view/head.php";
+include "view/loader.php";
+include "view/header.php";
+include 'view/banner.php';
+include "view/review.php";
+include "view/room.php";
+include "view/gallery.php";
+include "view/footer.php";
 ?>
