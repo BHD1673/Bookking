1fb0030112652f6bfef2c894b66af2d83676880a
BHD1673 <banhaiduong167@gmail.com>
2023-12-01 22:58:27 +0700

    Phần này không xóa

--- css/global.php
+++
@@ -1,3 +0,0 @@
-<?php
-$img_path = 'upload/';
-?>
--- css/model/sanphamtheodanhmuc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function loadall_sanphamtheodanhmuc_home()
-{
-  $ID = isset($_GET['ID']) ? $_GET['ID'] : 0;
-  $sql = "SELECT * FROM phong WHERE ID_LoaiPhong = $ID";
-  $listsptheodm = pdo_query($sql);
-  return $listsptheodm;
-}
